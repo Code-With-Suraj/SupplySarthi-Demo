8920d4753a79247e283200158f63a50e98ae05ea
suraj singh <surajsingh.noida98@gmail.com>
2026-08-18 14:15:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // SupplySarthi Frontend Configuration
 const CONFIG = {
   // Default/Fallback GAS_URL after deploying Code.gs as a Web App (Execute as: Me, Access: Anyone)
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwMpeGsUqQqDbKkhSEefqc5c_plhOP5NVFV9xRO2Sw0vEKERJzC_OXPBwK7qEDrJPt5/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwONONe8ZgV_Fg08h9iv4dnf72ygmZoiLM0AvLXkUH6BlHG450AsEyD0w_vPy8xjXkz/exec',
   RAZORPAY_KEY_ID: 'rzp_live_Sugpl07IegaqDU',
   MARKETPLACE_URL: 'https://supply-sarthi-market.vercel.app',
   APP_NAME: 'SupplySarthi'
